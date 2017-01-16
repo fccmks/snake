7c2c1fd452fee869b908061407a8e3de207743c8
Moiseev Konstantin <moiseevko@mail.ru>
2017-01-16 17:21:34 +0700

    Game window creation.

--- src/ru/geekbrains/mks/Snake.java
+++ src/ru/geekbrains/mks/Snake.java
@@ -1,9 +1,16 @@
 package ru.geekbrains.mks;
 
-public class Main {
+import javax.swing.*;
 
+public class Snake extends JFrame {
+    private static Snake game_window;
     public static void main(String[] args) {
-	// write your code here
-        System.out.println("Hello, World!!!");
+        game_window = new Snake();
+        game_window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        game_window.setSize(600, 500);
+        game_window.setLocation(750, 100);
+        game_window.setResizable(false);
+        game_window.setTitle("SNAKE");
+        game_window.setVisible(true);
     }
 }
