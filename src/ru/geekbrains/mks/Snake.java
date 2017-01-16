4759a8919042852462dc2a26d5a5804e626c743d
Moiseev Konstantin <moiseevko@mail.ru>
2017-01-16 17:55:26 +0700

    Trying to draw.

--- src/ru/geekbrains/mks/Snake.java
+++ src/ru/geekbrains/mks/Snake.java
@@ -1,6 +1,7 @@
 package ru.geekbrains.mks;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class Snake extends JFrame {
     private static Snake game_window;
@@ -11,6 +12,16 @@ public class Snake extends JFrame {
         game_window.setLocation(750, 100);
         game_window.setResizable(false);
         game_window.setTitle("SNAKE");
+        JPanel game_field = new JPanel();
+        game_field.paintComponents();
+        game_window.add(game_field);
         game_window.setVisible(true);
     }
+    public static void onRepaint(Graphics g) {
+        g.drawLine(10, 10, 80, 120);
+    }
+
+ //   public static class GameField extends JPanel {
+ //       protected void paintComponent
+ //   }
 }
